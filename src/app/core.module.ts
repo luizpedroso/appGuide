5f2b260c917d10be308377382560c9b201bf32bf
luizpedroso <luizcarlospedrosojr@gmail.com>
2019-09-23 12:07:10 -0300

    correcoes na injecao dos servicoes

--- src/app/core.module.ts
+++ src/app/core.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { RecipeService } from './recipes/recipe.service';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
 
 import { AuthInterceptorService } from './auth/auth.interceptor.service';
 
 @NgModule({
     providers: [
         RecipeService, 
-        ShoppingListService, 
         {
           provide: HTTP_INTERCEPTORS, 
           useClass: AuthInterceptorService, 
