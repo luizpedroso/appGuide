0506c99c5baaad362236c4d916effbb4b4615dac
luizpedroso <luizcarlospedrosojr@gmail.com>
2019-09-23 16:54:49 -0300

    complementos no redux

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,11 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { catchError, tap } from 'rxjs/operators';
 import { throwError, BehaviorSubject } from 'rxjs';
-
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
 import { environment } from '../../environments/environment';
 
 import { User } from './user.model';
-import { Router } from '@angular/router';
+import * as fromApp from '../store/app.reducer';
+import * as AuthActions from './store/auth.actions';
 
 export interface AuthResponseData{
     kind: string;
@@ -21,10 +23,10 @@ export interface AuthResponseData{
 @Injectable({providedIn: 'root'})
 export class AuthService{
 
-    user = new BehaviorSubject<User>(null);
+    //user = new BehaviorSubject<User>(null);
     private tokenExpirationTimer: any;
 
-    constructor(private http: HttpClient, private router: Router){}
+    constructor(private http: HttpClient, private router: Router, private store: Store<fromApp.AppState>){}
 
     signup(email: string, password: string){
         return this.http
@@ -88,7 +90,15 @@ export class AuthService{
         );
 
         if(loadedUser.token){
-            this.user.next(loadedUser);
+            //this.user.next(loadedUser);
+            this.store.dispatch(
+                new AuthActions.Login({
+                    email: loadedUser.email,
+                    user: loadedUser.id,
+                    token: loadedUser.token,
+                    expirationDate: new Date(userData._tokenExpirationDate)
+                })
+            );
             const expirationDuration =
                 new Date(userData._tokenExpirationDate).getTime() -
                 new Date().getTime();
@@ -97,7 +107,8 @@ export class AuthService{
     }
 
     logout(){
-        this.user.next(null);
+        //this.user.next(null);
+        this.store.dispatch(new AuthActions.Logout());
         this.router.navigate(['/auth']);
         localStorage.removeItem('userData');
         if(this.tokenExpirationTimer){
@@ -115,7 +126,15 @@ export class AuthService{
     private handleAuthentication(email: string, userId: string, token: string, expiresIn: number){
         const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
         const user = new User(email, userId, token, expirationDate);
-        this.user.next(user);
+        //this.user.next(user);
+        this.store.dispatch(
+            new AuthActions.Login({
+                email: email,
+                user: userId,
+                token: token,
+                expirationDate: expirationDate
+            })
+        );
         this.autoLogout(expiresIn * 1000);
         localStorage.setItem('userData', JSON.stringify(user));
     }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit, OnDestroy } from "@angular/core";
 import { Subscription } from 'rxjs';
+import { Store } from '@ngrx/store';
 
 import { DataStorageService } from '../shared/data-storage.service';
 import { AuthService } from '../auth/auth.service';
-
+import * as fromApp from '../store/app.reducer';
+import { map } from 'rxjs/operators';
 
 @Component({
     selector: 'app-header',
@@ -13,10 +15,20 @@ import { AuthService } from '../auth/auth.service';
 export class HeaderComponent implements OnInit, OnDestroy {
     isAuthenticated: boolean = false;
     private userSub: Subscription; 
-    constructor(private dataStorageService: DataStorageService, private authService: AuthService){}
+    constructor(
+        private dataStorageService: DataStorageService, 
+        private authService: AuthService, 
+        private store: Store<fromApp.AppState>
+    ){}
     
     ngOnInit(){
-        this.userSub = this.authService.user.subscribe(user =>{
+        this.userSub = this.store.select('auth')
+        .pipe(
+            map(authState => {
+                return authState.user;
+            })
+        )
+        .subscribe(user =>{
             this.isAuthenticated = !!user;
         });
     }
